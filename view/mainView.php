9ee485eb0db8cf2745c44b0d695d65b49fe6978b
Joey Sagnes <dragonskymine@gmail.com>
2017-10-17 14:35:00 +0200

    update mainView with bootstrap

--- view/mainView.php
+++ view/mainView.php
@@ -1,37 +1,43 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN"
-"http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+	<title>Site SIL3</title>
+	<meta charset="utf-8">
+	<link rel="stylesheet" href="view/lib/botstrap/css/bootstrap.min.css">
+	<link rel="stylesheet" href="view/lib/font-awesome/css/font-awesome.min.css">
+	<link rel="stylesheet" type="text/css" href="view/style.css" media="screen" title="Normal" />
+	<script src="view/lib/jquery.min.js" charset="utf-8"></script>
+	<script src="view/lib/bootstrap/js/bootstrap.min.js" charset="utf-8"></script>
+</head>
 
-<html xmlns="http://www.w3.org/1999/xhtml"  xml:lang="fr" >
-	<head>
-		<title>Site SIL3</title>
-		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-		<link rel="stylesheet" type="text/css" href="view/style.css" media="screen" title="Normal" />
-	</head>
-	
-	<body>
-		<div id="entete">
-			<h1>Site SIL3</h1>
-		</div>
-		
-		<div id="menu">		
+<body>
+	<header class="container">
+		<h1>Site SIL3</h1>
+	</header>
+
+	<div class="container">
+
+		<nav>
 			<h3>Menu</h3>
-			<ul>
-				<?php 
-					# Mise en place du menu par un parcours de la table associative
-					foreach ($data["menu"] as $item => $act) {
-				?>
-				<li class="list-group-item"><a href="<?= $act ?>"><?= $item ?></a></li>
+			<ul class="list-group">
 				<?php
-					}
+				# Mise en place du menu par un parcours de la table associative
+				foreach ($data["menu"] as $item => $act) {
+					?>
+					<li class="list-group-item"><a href="<?= $act ?>"><?= $item ?></a></li>
+					<?php
+				}
 				?>
 			</ul>
-		</div>
-		
-		<div id="corps">
+		</nav>
+
+		<div class="col-md-9" id="corps">
 			<?php require_once $data["view"]; ?>
 		</div>
-		
-		<div id="pied_de_page">
-		</div>
-	</body>
+
+	</div>
+
+	<div id="pied_de_page">
+	</div>
+</body>
 </html>
