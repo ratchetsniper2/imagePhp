b5456332ca23aa33fe0c590a2bf33a2d93fb6bca
Joey Sagnes <dragonskymine@gmail.com>
2017-11-06 09:12:03 +0100

    add photoMatrixView

--- controller/photoMatrix.php
+++ controller/photoMatrix.php
@@ -1,9 +1,74 @@
 <?php
 
+require_once("model/image.php");
+require_once("model/imageDAO.php");
+
 class PhotoMatrix {
-		
-	public function indexAction(){
-		
+
+	private $imgDAO;
+
+	public function __construct(){
+		$this->imgDAO = new ImageDAO();
 	}
-	
+
+	private function getData(array $imgs, int $nbImg = null) : array{
+
+		if ($nbImg == null){
+			// Si il n'y a pas de taille demandé, on garde la meme taille ou celle par defaut
+			if (isset($_GET["nbImg"]) && is_numeric($_GET["nbImg"])) {
+				$nbImg = $_GET["nbImg"];
+			} else {
+				$nbImg = 2;
+			}
+		}
+
+		foreach ($imgs as $key => $value) {
+			$data["img"][$key]["imgUrl"] = $value->getURL();
+			$data["img"][$key]["imgId"] = $value->getId();
+			$data["img"][$key]["imgComment"] = $value->getComment();
+			$data["img"][$key]["imgCategory"] = $value->getCategory();
+		}
+
+		// menu
+		$imgId = $imgs[array_keys($imgs)[0]]->getId();
+
+		$nbImgBis = $nbImg*2;
+
+		$data["menu"]['Home'] = "index.php";
+		$data["menu"]['A propos'] = "index.php?controller=home&action=aproposAction";
+		$data["menu"]['First'] = "index.php?controller=photoMatrix&action=firstAction";
+		$data["menu"]['Random'] = "index.php?controller=photoMatrix&action=randomAction&imgId=$imgId";
+		$data["menu"]['More'] = "index.php?controller=photoMatrix&imgId=$imgId&nbImg=$nbImgBis";
+
+		return $data;
+	}
+
+	public function indexAction() {
+		$this->showAction();
+	}
+
+	public function showAction() {
+		if (isset($_GET["imgId"]) && is_numeric($_GET["imgId"])) {
+			$imgId = $_GET["imgId"];
+			$img = $this->imgDAO->getImage($imgId);
+		} else {
+			// Pas d'image, se positionne sur la première
+			$img = $this->imgDAO->getFirstImage();
+		}
+
+		if (isset($_GET["nbImg"]) && is_numeric($_GET["nbImg"])) {
+			$nbImg = $_GET["nbImg"];
+		} else {
+			$nbImg = 2;
+		}
+
+		$imgs = $this->imgDAO->getImageList($img, $nbImg);
+
+
+		$data = $this->getData($imgs, $nbImg);
+		$data["view"] = "photoMatrixView.php";
+
+		require_once("view/mainView.php");
+	}
+
 }
--- view/photoMatrixView.php
+++ view/photoMatrixView.php
@@ -1,18 +1,18 @@
-<?php # mise en place de la vue partielle : le contenu central de la page  
-	# Mise en place des deux boutons
-	print "<p>\n";
-	// pre-calcul de la page d'images précedente
-	print "<a href=\"nonRealise.php\">Prev</a> ";
-	// pre-calcul de la page d'images suivante
-	print "<a href=\"nonRealise.php\">Next</a>\n";
-	print "</p>\n";
-	# Affiche de la matrice d'image avec une reaction au click
-	print "<a href=\"zoom.php?zoom=1.25&imgId=$newImgId&size=$size\">\n";
-	// Réalise l'affichage de l'image
-	# Adapte la taille des images au nombre d'images présentes
-	$size = 480 / sqrt(count($imgMatrixURL));
-	# Affiche les images
-	foreach ($imgMatrixURL as $i) {
-		print "<a href=\"".$i[1]."\"><img src=\"".$i[0]."\" width=\"".$size."\" height=\"".$size."\"></a>\n";
-	};
-?>
+<div class="panel panel-default">
+
+	<div class="panel-heading">
+		<a class="btn btn-default" href="index.php?controller=photo&action=prevAction&imgId=<?= $data["imgId"] ?>&size=<?= $data["imgSize"] ?>"><i class="fa fa-arrow-left" aria-hidden="true"></i> Prev</a>
+		<a class="btn btn-default" href="index.php?controller=photo&action=nextAction&imgId=<?= $data["imgId"] ?>&size=<?= $data["imgSize"] ?>">Next <i class="fa fa-arrow-right" aria-hidden="true"></i></a>
+	</div>
+
+	<div class="panel-body row">
+		<?php foreach ($data["img"] as $img) { ?>
+			<div class="col-md-4">
+				<a class="thumbnail" href="index.php?controller=photo&action=zoomAction&zoom=1.25&imgId=<?= $img["imgId"] ?>">
+					<img src="<?php echo $img["imgUrl"]; ?>">
+				</a>
+			</div>
+		<?php } ?>
+	</div>
+
+</div>
