6c7e28ba57adf415fa1e8d14cdeef9f7a5ce0f7b
ratchetsniper2 <ratchetsniper2@gmail.com>
2017-11-07 08:46:45 +0100

    update home and a propos pages

--- view/aproposView.php
+++ view/aproposView.php
@@ -1,5 +1,6 @@
 <div class="page-header">
 	<h1>Information !</h1>
 </div>
-<p class="lead">Cette application  a pour but de mettre en pratique  le modèle MVC en PHP par l'équipe de SIL3 de l'IUT2 de Grenoble.</p>
-<p>Cette version utilise les images sur le disque</p>
+<p class="lead">Cette application a pour but de mettre en pratique le modèle MVC en PHP.</p>
+<p>Crée par Bouchet Valentin et Sagnes Joey</p>
+<p>Cette version utilise les images sur le disque et une base de données MySQL</p>
--- view/homeView.php
+++ view/homeView.php
@@ -2,4 +2,4 @@
 	<h1>Bonjour !</h1>
 </div>
 <p class="lead">Cette application vous permet de manipuler des photos</p>
-<p>Vous pouvez : naviguer, partager, classer en album</p>
+<p>Vous pouvez : naviguer, éditer, classer par catégorie</p>
